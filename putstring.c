b27a4b58c91b71798fcbdce279ca2ed3b136c9f1
sado1111 <sadohannahfabohns@gmail.com>
2023-08-23 01:40:50 +0100

    just creatted puts function that returns total numbers of charcetrs
    "
    @
    #

---
+++ putstring.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - thoroughly prints strings to stdout as puts
+ * @s: pointer to strings to be written/printed to stdout
+ * Return: total characters printed by _puts function to stdout
+ */
+
+int _puts(char *s)
+{
+	int char_count = 0;
+
+	while (*str)
+	{
+		_putchar(*str);
+		char_count++;
+		str++;
+	}
+
+	return (printed_count);
+
+}
